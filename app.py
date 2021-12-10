43ebd47767d93294cb2547a76ea55366c6507705
Julita K <julitakuczkowska@gmail.com>
2021-12-10 01:22:18 +0100

    function random and category ready

--- app.py
+++ app.py
@@ -14,15 +14,22 @@ def index():
 def all():
     # TODO return categories
     categories = category.category_list 
-    print('all categories available:', category.category_list)
-    return str(categories)
+    print('all categories available:', categories)
+    #return str(categories)
+    return render_template("category.html",
+                            categories=categories
+                            )
 
 
 @app.route('/category/<category_name>')
 def category_recepies(category_name):
-    print('category chosen:', category_name )
+    print('category chosen:', category_name)
     recepies = category.get_meals_by_category(category_name)
-    return recepies
+    #return recepies
+    return render_template("ucategory.html",
+                            recepies=recepies,
+                            category = category_name
+                            )
 
 
 @app.route('/id/<recepie_id>')
@@ -30,7 +37,25 @@ def recepie_by_id(recepie_id):
     print('recepie_id chosen:', recepie_id )
     meal = Meal(recepie_id)
     recepie = meal.get_meal_by_id(recepie_id)
-    return recepie
+    
+    instruction = (recepie['meals'][0]['strInstructions'])
+    instruction = instruction.split('\n')
+    print(instruction)
+
+    def get_ingredients(recepie):
+        ingredients = {}
+        for i in range(20): 
+            i = i + 1
+            ingredients[(recepie['meals'][0]['strIngredient'+str(i)])] = [(recepie['meals'][0]['strMeasure'+str(i)])]
+        return ingredients
+
+    ingredients = get_ingredients(recepie)
+
+    return render_template("recepie.html",
+                            recepie = recepie,
+                            instruction = instruction,
+                            ingredients=ingredients
+                            )
 
 @app.route('/random')
 def random():
@@ -51,9 +76,6 @@ def random():
         return ingredients
 
     ingredients = get_ingredients(recepie)
-    yt_link = meal.get_yt_link(recepie)
-    area = meal.get_area(recepie)
-    category = meal.get_category(recepie)
 
     return render_template("recepie.html",
                             recepie = recepie,
