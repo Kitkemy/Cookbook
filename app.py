7e2df7f4e6b0fb06c2a9e900e8b64fbc1fafb5e8
Julita K <julitakuczkowska@gmail.com>
2021-12-15 22:53:38 +0100

    final project for future collars

--- app.py
+++ app.py
@@ -1,110 +1,80 @@
-from flask import Flask, render_template, request
 from requests.api import post
-from .cookbook import Meal, Category, Ingredients, MEALDB_API_URL
+from flask import Flask, render_template, request
+from .cookbook import Meal, Category, Ingredients
 
 app = Flask(__name__)
 
 category = Category()
 ingredients = Ingredients()
 
-#tutaj zdefiniować funkcje
 def get_ingredients(recepie):
     ingredients = {}
     for i in range(1,20):
-        if recepie['meals'][0]['strMeasure'+str(i)].strip():
-            ingredients[(recepie['meals'][0]['strIngredient'+str(i)])] = [(recepie['meals'][0]['strMeasure'+str(i)])]
+        ingredient_amount = recepie['meals'][0]['strMeasure'+str(i)].strip()
+        if ingredient_amount:
+            ingredient_name = recepie['meals'][0]['strIngredient'+str(i)]
+            ingredients[ingredient_name] = ingredient_amount
     return ingredients
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    print(request.form.get('ingredient'))
-    ingredient = request.form.get('ingredient')
+def get_recepie_response(recepie):
+        instruction = (recepie['meals'][0]['strInstructions'])
+        instruction = instruction.split('\n')
+        ingredients = get_ingredients(recepie)
+        yt_link1 = recepie['meals'][0]['strYoutube']
+        yt_link = yt_link1.replace("watch?v=","embed/")
 
-    return render_template("index2.html",
-                           ingredient=ingredient
-                            )
+        return render_template("recepie.html",
+                                recepie = recepie,
+                                instruction = instruction,
+                                ingredients=ingredients,
+                                yt_link=yt_link
+                                )
 
-@app.route('/recepie2', methods=["GET", "POST"])
-def aaa():
-    print(request.form.get('ingredient'))
-    ingredient = request.form.get('ingredient')
+def recepie_random():
+      meal = Meal("random")
+      recepie = meal.get_random_data()
+      return recepie
 
-    return render_template("recepie2.html",
-                           ingredient=ingredient
-                            )
+@app.route('/')
+def index():    
+    return render_template("index2.html")
+
+@app.route('/ingredient')
+def ingrendient():
+    print('meal by ingrendient chosen')
+    meals_list = ingredients.get_meals_by_ingredient(request.args.get('ingredient'))
+    return render_template("ucategory.html",
+                            recepies=meals_list
+                            )                            
 
 @app.route('/category/all')
-#def index(category):
 def all():
-    # TODO return categories
     categories = category.category_list 
     print('all categories available:', categories)
-    #return str(categories)
     return render_template("category.html",
                             categories=categories
                             )
-
 
 @app.route('/category/<category_name>')
 def category_recepies(category_name):
     print('category chosen:', category_name)
     recepies = category.get_meals_by_category(category_name)
-    #return recepies
     return render_template("ucategory.html",
                             recepies=recepies
                             )
 
-
 @app.route('/id/<recepie_id>')
 def recepie_by_id(recepie_id):
-    print('recepie_id chosen:', recepie_id )
-    meal = Meal(recepie_id)
-    recepie = meal.get_meal_by_id(recepie_id)
-    
-    instruction = (recepie['meals'][0]['strInstructions'])
-    instruction = instruction.split('\n')
-    print(instruction)
 
-    ingredients = get_ingredients(recepie)
+    def recepie_by_id(recepie_id):
+      meal = Meal(recepie_id)
+      recepie = meal.get_meal_by_id(recepie_id)
+      return recepie
 
-    yt_link1 = recepie['meals'][0]['strYoutube']
-    yt_link = yt_link1.replace("watch?v=","embed/")
-
-    return render_template("recepie.html",
-                            recepie = recepie,
-                            instruction = instruction,
-                            ingredients=ingredients,
-                            yt_link=yt_link
-                            )
+    recepie = recepie_by_id(recepie_id)        
+    return get_recepie_response(recepie)
 
 @app.route('/random')
 def random():
-    print('random meal chosen')
-    meal = Meal("random")
-    recepie = meal.get_random_data()
-
-    
-    instruction = (recepie['meals'][0]['strInstructions'])
-    instruction = instruction.split('\n')
-    print(instruction)      
-
-    ingredients = get_ingredients(recepie)
-
-    yt_link1 = recepie['meals'][0]['strYoutube']
-    yt_link = yt_link1.replace("watch?v=","embed/")
-
-    return render_template("recepie.html",
-                            recepie = recepie,
-                            instruction = instruction,
-                            ingredients=ingredients,
-                            yt_link=yt_link
-                            )
-
-@app.route('/ingredient/<ingredient_name>')
-def ingrendient(ingredient_name):
-    print('meal by ingrendient chosen')
-    meals_list = ingredients.get_meals_by_ingredient(ingredient_name)
-    return render_template("ucategory.html",
-                            recepies=meals_list
-                            )
-    
+    recepie = recepie_random()        
+    return get_recepie_response(recepie)
--- cookbook.py
+++ cookbook.py
@@ -8,23 +8,7 @@ MEALDB_API_URL = "http://www.themealdb.com/api/json/v1/1/"
 class Meal:
     def __init__(self, meal_selection):
         self.meal_selection = meal_selection
-        #self.data = self.get_data()
-        #self.name = self.get_name()
-        #self.img_path = self.get_img_path()
-        #self.instruction = self.get_instruction()
-        #self.ingredients = self.get_ingredients()
-        #self.yt_link = self.get_yt_link()
-        #self.area = self.get_area()
-        #self.category = self.get_category()
-        
-    '''def get_data(self):
-        if self.meal_selection == "random":
-            response = self.get_random_data()
-        elif self.meal_selection == "id":
-            response = self.get_meal_by_id(id)
-            #metoda dla category i dalej kolejna po głównym składniku
-        return response
-    '''    
+
     def get_random_data(self):
             r = requests.get(f'{MEALDB_API_URL}/random.php')
             response = r.json()
@@ -68,10 +52,6 @@ class Meal:
         category =  recepie['meals'][0]['strCategory']
         return category
 
-
-
-
-
 class Category:
     def __init__(self):
         self.category_list = self.get_category_list()
@@ -80,7 +60,6 @@ class Category:
         
         category_list = requests.get(f'{MEALDB_API_URL}/list.php?c=list')
         response = category_list.json()
-        #pprint.pprint(response)
         return self.create_list(response)
 
     
@@ -95,18 +74,13 @@ class Category:
         
         meals_list = requests.get(f'{MEALDB_API_URL}/filter.php?c={category_name}')
         response = meals_list.json()
-        #pprint.pprint(response)
         return response
 
-category = Category()
-#print(category.category_list)
+
 
 
 
 class Ingredients:
-    def __init__(self):
-        #self.list = self.get_ingredients_list()
-        pass 
 
     def get_ingredients_list(self):
         
@@ -131,90 +105,41 @@ class Ingredients:
         return list
 
 
-'''
-class Areas:
-    def __init__(self):
-        self.areas_list = self.get_areas_list()
-        
-    def get_areas_list(self):
-        
-        areas_list = requests.get('{MEALDB_API_URL}/list.php?a=list')
-        response = areas_list.json()
-        pprint.pprint(response)
-        return self.create_list(response)
+# not yet used
+#class Areas:
+#    def __init__(self):
+#        self.areas_list = self.get_areas_list()
+#        
+#    def get_areas_list(self):
+#        
+#        areas_list = requests.get('{MEALDB_API_URL}/list.php?a=list')
+#        response = areas_list.json()
+#        pprint.pprint(response)
+#        return self.create_list(response)
+#
+#    
+#    def create_list(self, areas_list):
+#        list = []
+#        length = len(areas_list['meals'])
+#        for i in range(length):
+#            list.append(areas_list['meals'][i]['strArea'])
+#        return list
 
-    
-    def create_list(self, areas_list):
-        list = []
-        length = len(areas_list['meals'])
-        for i in range(length):
-            list.append(areas_list['meals'][i]['strArea'])
-        return list
-
-class Ingredients:
-    def __init__(self):
-        self.ingredients_list = self.get_ingredients_list()
-        
-    def get_ingredients_list(self):
-        
-        ingredients_list = requests.get('{MEALDB_API_URL}/list.php?i=list')
-        response = ingredients_list.json()
-        pprint.pprint(response)
-        return self.create_list(response)
-
-    
-    def create_list(self, ingredients_list):
-        list = []
-        length = len(ingredients_list['meals'])
-        for i in range(length):
-            list.append(ingredients_list['meals'][i]['strIngredient'])
-        return list
-
-areas = Areas()
-print(areas.areas_list)
-
-ingredients = Ingredients()
-print(ingredients.ingredients_list)
-'''
-#czy tego nie zrobić w klasę starter?
-'''choice = "Vegetarian"
-
-if choice == "random":
-    meal = Meal("random")
-    #test:
-    print(meal.name)
-    print('\n')
-    print(meal.img_path)
-    print('\n')
-    print(meal.instruction)
-    print('\n')
-    print(meal.ingredients)
-    print(meal.yt_link)
-    print(meal.area)
-    print(meal.category)
-
-#looking meals by category
-elif choice in category.category_list:
-    print(f'user choose option CATEGORY: {choice}')
-    list_meals = category.get_meals_by_category(choice)
-    
-    #print(list_meals)
-
-    id = input('select meal ID: ')
-
-    meal = Meal("id")
-   ''' 
-
-
-
-#yutube i category add, area - DONE
-#dodac tworzenie posiłkow po kategoriach - DONE ale czy tak może być?????? bo na tej podstawie stworzę 2 kolejne punkty
-#tworzenie posiłkow po głownym skladniku
-#tworzenie posiłkow po area
-
-#dodac szukanie po area
-#dodac szukanie po kategoriach - DONE
-#szukanie po głownym skladniku
-
-#zaczac  flaska- zeby dane wpisane recznie wyswietlily sie na stronie
-
+# not yet used
+#class Ingredients:
+#    def __init__(self):
+#        self.ingredients_list = self.get_ingredients_list()
+#        
+#    def get_ingredients_list(self):
+#        
+#        ingredients_list = requests.get('{MEALDB_API_URL}/list.php?i=list')
+#        response = ingredients_list.json()
+#        pprint.pprint(response)
+#        return self.create_list(response)
+#     
+#    def create_list(self, ingredients_list):
+#        list = []
+#        length = len(ingredients_list['meals'])
+#        for i in range(length):
+#            list.append(ingredients_list['meals'][i]['strIngredient'])
+#        return list
